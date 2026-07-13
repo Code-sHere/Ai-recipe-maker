2cdaa2081fe202f663c1d97ccdb7c1dcea771d3c
Code-sHere <anshjaat102010@gmail.com>
2026-07-13 22:09:19 +0530

    Convert servdfrontend from submodule to normal folder

---
+++ servdfrontend/components/PricingSection.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { PricingTable } from '@clerk/nextjs'
+
+const PricingSection = () => {
+    return (
+        <div className='max-4xl '>
+            <div className="mb-16">
+                <h2 className="text-5xl md:text-6xl font-bold mb-4">Pricing Table</h2>
+                <p className="text-xl text-stone-600 font-light">Start for free. Upgrade to a paid plan to get more features.</p>
+            </div>
+            <div>
+                <PricingTable
+                    checkoutProps={{
+                        appearance: {
+                            elements: {
+                                drawerRoot: {
+                                    zIndex: 2000,
+                                }
+                            }
+                        }
+                    }}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default PricingSection
