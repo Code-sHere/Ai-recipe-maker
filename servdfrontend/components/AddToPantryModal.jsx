3c05b5b8dde4a66bab20da32ea16cce6e26feb52
Code-sHere <anshjaat102010@gmail.com>
2026-07-21 22:18:17 +0530

    adding new features

--- servdfrontend/components/AddToPantryModal.jsx
+++ servdfrontend/components/AddToPantryModal.jsx
@@ -5,6 +5,7 @@ import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle, Dialog
 import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs'
 import { Plus } from 'lucide-react'
 import { Camera } from 'lucide-react'
+import useFetch from '@/hooks/use-fetch'
 
 
 const AddToPantryModal = ({ isOpen, onClose, onSuccess }) => {
@@ -17,6 +18,25 @@ const AddToPantryModal = ({ isOpen, onClose, onSuccess }) => {
     const [scannedIngredients, setScannedIngredients] = useState([]);
 
     const [manualItem, setManualItem] = useState({ name: "", quntity: "" });
+
+    const {
+        loading: scanning,
+        data: scanData,
+        fetchData: scanImage,
+    } = useFetch(scanPantryItem);
+
+    const {
+        loading: saving,
+        data: saveData,
+        fetchData: saveSacnnedItem,
+    } = useFetch(saveToPantry);
+
+    const {
+        loading: adding,
+        data: addData,
+        fetchData: addManualItem,
+    } = useFetch(addToPantryItemManually);
+
 
     const handleClose = () => {
         setActiveTab("scan");
@@ -58,7 +78,27 @@ const AddToPantryModal = ({ isOpen, onClose, onSuccess }) => {
                     <TabsContent value="manually" className="mt-6"><form onSubmit={handleAddManual} className="space-y-6">
                         <div>
                             <label classname="block text-sm font-medium text-stone-700 mb-2">Ingredient Name</label>
-                        </div>    
+                            <input 
+                            type="text"
+                            valu={manualItem.name}
+                            onChange={(e)=> setManualItem({...manualItem, name: e.target.value})}
+                            placeholder='e.g., Dal Makhini'
+                            className='w-full px-4 py-3 border border-stone-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-orange-600'
+                            diabled={adding}
+                            />
+                        </div> 
+
+                        <div>
+                            <label className='block text-sm font-medium text-stone-700 mb-2'>Quantity</label>
+                            <input 
+                            type="text"
+                            valu={manualItem.quntity}
+                            onChange={(e)=> setManualItem({...manualItem, quntity: e.target.value})}
+                            placeholder='e.g., 1 cup'
+                            className='w-full px-4 py-3 border border-stone-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-orange-600'
+                            diabled={adding}
+                            />
+                        </div>   
                     </form></TabsContent>
                 </Tabs>
             </DialogContent>
